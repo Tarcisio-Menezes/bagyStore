96e0e357e23204ee25cf0b3f051ca4719dadd56e
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-29 12:34:31 -0300

    edita config.js do sequelize

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,28 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: 'bagyStore',
+    host: process.env.HOST,
+    dialect: 'sqlite',
+    databaseUrl: process.env.DATABASE_URL,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: 'bagyStore',
+    host: process.env.HOST,
+    dialect: 'sqlite',
+    databaseUrl: process.env.DATABASE_URL,
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: 'bagyStore',
+    host: process.env.HOST,
+    dialect: 'sqlite',
+    databaseUrl: process.env.DATABASE_URL,
+  },
+};
