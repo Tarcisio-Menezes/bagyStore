b92f5a70c1240185dc64a095c750ca3c315364cc
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-29 14:26:25 -0300

    edita model order

--- database/models/order.js
+++ database/models/order.js
@@ -1,27 +1,23 @@
-const {
-  Model,
-} = require('sequelize');
-
+// eslint-disable-next-line max-lines-per-function
 module.exports = (sequelize, DataTypes) => {
-  class Order extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Order.init({
-    products: DataTypes.STRING,
+  const Order = sequelize.define('Order', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     date: DataTypes.STRING,
     parcel: DataTypes.NUMBER,
-    client: DataTypes.STRING,
     status: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'Order',
+  },
+  {
+    timestamps: false,
+    tableName: 'Orders',
+    underscored: true,
   });
+
+  Order.associate = (models) => {
+    Order.hasMany(models.product,
+      { foreignKey: 'id', as: 'order_id' });
+    Order.belongsTo(models.client,
+      { foreignKey: 'id', as: 'client_id' });
+  };
+
   return Order;
 };
